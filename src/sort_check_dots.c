eba4ee259c7058233986a1f94e61808e0978ba60
ng <girisn00@gmail.com>
2020-06-11 21:23:03 +0300

    -a complete

--- src/sort_check_dots.c
+++ src/sort_check_dots.c
@@ -1,6 +1,6 @@
 #include "ft_ls.h"
 
-int		ft_strcmp_abc(char *s1, char *s2)
+int		ft_strcmp_abc(char *s1, char *s2, int n)
 {
 	int		i;
 	int		dot;
@@ -12,7 +12,8 @@ int		ft_strcmp_abc(char *s1, char *s2)
 		&& s1[i] - s2[i] > -32 && s1[i] - s2[i] <= -7)
 		return (1);
 	else if (s1[i] >= 'a' && s1[i] <= 'z' && s2[i] >= 'A' && s2[i] <= 'Z'
-		&& s1[i] - s2[i] < 32 && s1[i] - s2[i] >= 7)
+		&& ((s1[i] - s2[i] < 32 && s1[i] - s2[i] >= 7)
+		|| (n == 1 && s1[i] - s2[i] == 32)))
 		return (-1);
 	else if (s1[i] > s2[i])
 		return (1);
@@ -32,18 +33,22 @@ int		check_dots(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	if (s1[0] != '.' && s2[0] != '.')
-		return (ft_strcmp_abc(s1, s2));
+		return (ft_strcmp_abc(s1, s2, 0));
 	if (!ft_strcmp(s1, ".") && !ft_strcmp(s2, ".."))
-		return (1);
-	if (!ft_strcmp(s2, ".") && !ft_strcmp(s1, ".."))
 		return (-1);
+	if (!ft_strcmp(s2, ".") && !ft_strcmp(s1, ".."))
+		return (1);
 	while (s1[i] == '.')
 		i++;
 	t1 = s1 + i;
 	while (s2[j] == '.')
 		j++;
 	t2 = s2 + j;
-	if ((n = ft_strcmp_abc(t1, t2)) > 0)
+	if ((s1[0] == '.' && s2[0] != '.') || (s1[0] != '.' && s2[0] == '.'))
+		n = ft_strcmp_abc(t1, t2, 1);
+	else
+		n = ft_strcmp_abc(t1, t2, 0);
+	if (n > 0)
 		return (1);
 	else if (n < 0)
 		return (-1);
